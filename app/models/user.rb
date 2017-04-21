daaaa445a359f94efea9827959c551f4ebc1a4da
dylanrainwater <dylanrainwater98@gmail.com>
2017-04-21 15:15:13 -0400

    Validate user Name and Email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
+  validates :name, presence: true, length: { maximum: 50}
+  # Regex matches valid emails:
+  # (any word, +, -, or .)@(at least a letter, -, #, or .).(at least a letter)
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+                    format: {with: EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
 end
