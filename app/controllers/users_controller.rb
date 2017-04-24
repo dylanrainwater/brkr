7c67c5fc63e1d052f797c38d83d25e7720eed8af
dylanrainwater <dylanrainwater98@gmail.com>
2017-04-24 13:15:49 -0400

    Add change settings option

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.all
+    
+  end
 
   def show
     @user = User.find(params[:id])
@@ -13,8 +20,21 @@ class UsersController < ApplicationController
     redirect_to current_user
   end
 
-  def create
+  def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Successfully edited!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def create
     @user = User.new(user_params)
     if @user.save
       @user.update_attribute(:bio, "#{@user.name} hasn't made a bio yet!")
@@ -24,6 +44,19 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Sign up or log in before continuing"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 
   private
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,42 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:test_user)
+    @wrong_user = users(:other_user)
+  end
+
   test "should get signup" do
     get signup_path
     assert_response :success
+  end
+
+  test "should redirect edit when logged out" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when logged out" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@wrong_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@wrong_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 
 end
